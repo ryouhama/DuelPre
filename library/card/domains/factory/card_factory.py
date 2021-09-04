37dd84cedefea7e022a3e33371a0fdb3ca5348d1
ryouhama <hary0730cap@icloud.com>
2021-09-05 00:11:09 +0900

    add adapter

---
+++ library/card/domains/factory/card_factory.py
@@ -0,0 +1,22 @@
+from library.card.domains.domain.card.value_object import CardId
+from typing import Any, Dict, List
+from library.card.domains.domain.card.domain import Card, Civilization, Civilizations
+
+
+class CardFactory():
+    def create(self, data: Dict[str, Any]) -> Card:
+        return Card(
+            id=CardId(data['id']),
+            name=data['name'],
+            cost=data['cost'],
+            effect_document=data['effect_document'],
+            picture_path=data['picture_path'],
+            civilizations=CivilizationsFactory(data)
+        )
+
+class CivilizationsFactory():
+    def create(self, data: Dict[str, Any]) -> Civilizations:
+        civilizations_data: List[str] = data['civilizations']
+        return Civilizations(
+            items=[Civilization.to_domain(it) for it in civilizations_data]
+        )
